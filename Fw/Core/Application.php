adf2fcc6c5d5522d684312d300c94a7129b96390
artemmm312 <artemmm312@gmail.com>
2023-04-19 22:31:30 +0300

    второй этап, немного переделал Application и Page

--- Fw/Core/Application.php
+++ Fw/Core/Application.php
@@ -2,110 +2,58 @@
 
 namespace Fw\Core;
 
-use Exception;
-use Fw\Core\Config;
-use Fw\Core\Page;
-
 final class Application
 {
-	private static ?Page $pager = null; // будет объект класса
+	private static ?Page $pager = null;
 	private $template = null; //будет объект класса
 	
 	public function __construct()
 	{
 	}
+	
 	public function getPage(): Page
 	{
-		if(empty(self::$pager)) {
+		if (empty(self::$pager)) {
 			self::$pager = new Page();
 		}
 		return self::$pager;
 	}
 	
-	private function startBuffer()
+	private function startBuffer(): void
 	{
 		ob_start();
 	}
 	
-	private function endBuffer()
+	private function endBuffer(): void
 	{
 		$content = ob_get_contents();
-		$replaceable = $this->getPage()->getAllReplace();
-		
-		$js = '';
-		foreach ($replaceable['js'] as $src) {
-			$js .= '<script async src="' . $src . '"></script>';
-		}
-		$content = str_replace('#FW_MACRO_JS#', $js, $content);
-		
-		$css = '';
-		foreach ($replaceable['css'] as $link) {
-			$css .= '<link href="' . $link . '" type="text/css" rel="stylesheet">';
-		}
-		$content = str_replace('#FW_MACRO_CSS#', $css, $content);
-		
-		$string = implode('', $replaceable['string']);
-		$content = str_replace('#FW_MACRO_STRING#', $string, $content);
-		
-		foreach ($replaceable['properties'] as $id => $value) {
-			$content = str_replace("#FW_PAGE_PROPERTY_$id#", $value, $content);
-		}
-		
-		/*$content = str_replace(['#FW_MACRO_CSS#', '#FW_MACRO_TITLE#', '#FW_MACRO_DESCRIPTION#', '#FW_MACRO_KEYWORDS#'],
-			['<link rel="stylesheet" type="text/css" href="style.css">', 'Мой чудесный тайтл', 'Мое описание', 'ключевые слова, мета-тег, SEO'],
-			$content);*/
+		$content = $this->getPage()->getAllReplace($content);
 		ob_end_clean();
 		echo $content;
 	}
 	
-	public function restartBuffer()
+	public function restartBuffer(): void
 	{
 		ob_clean();
 	}
 	
-	public function header()
+	public function header(): void
 	{
 		$this->startBuffer();
 		$template = Config::get('templates/main') . 'header.php';
 		if (!file_exists($template)) {
-			throw new \Exception("Файл '{$template}' не существует");
+			throw new \RuntimeException("Файл '{$template}' не существует");
 		}
 		include $template;
 	}
 	
-	public function footer()
+	public function footer(): void
 	{
 		$template = Config::get('templates/main') . 'footer.php';
 		if (!file_exists($template)) {
-			throw new \Exception("Файл '{$template}' не существует");
+			throw new \RuntimeException("Файл '{$template}' не существует");
 		}
 		include $template;
 		$this->endBuffer();
 	}
-	
-	
-	/*	private static ?Application $instance = null;
-		private $pager = null; // будет объект класса
-		private $template = null; //будет объект класса
-		
-		private function __construct()
-		{
-		}
-		
-		public static function getInstance(): Application
-		{
-			if (empty(self::$instance)) {
-				self::$instance = new self();
-			}
-			return self::$instance;
-		}
-		
-		private function __clone()
-		{
-		}
-		
-		public function __wakeup(): void
-		{
-			throw new Exception("Cannot unserialize a singleton.");
-		}*/
 }
--- Fw/Core/Page.php
+++ Fw/Core/Page.php
@@ -4,59 +4,129 @@ namespace Fw\Core;
 
 class Page
 {
+	private string $title = '';
+	private string $description = '';
+	private string $keywords = '';
 	private array $js = [];
 	private array $css = [];
 	private array $string = [];
-	
 	private array $properties = [];
+	private array $macros =
+		[
+			'head' =>
+				[
+					'title' => "#FW_MACRO_TITLE#",
+					'description' => "#FW_MACRO_DESCRIPTION#",
+					'keywords' => "#FW_MACRO_KEYWORDS#",
+					'js' => "#FW_MACRO_JS#",
+					'css' => "#FW_MACRO_CSS#",
+					'string' => "#FW_MACRO_STRING#",
+				],
+		];
 	
-	public function addJs(string $src)  //добавляет src в массив сохраняя уникальность
+	public function setTitle(string $title): void
 	{
-		$this->js[] = $src;
+		$this->title = $title;
 	}
 	
-	public function addCss(string $link)  //добавляет link сохраняя уникальность
+	public function setDescription(string $description): void
 	{
-		$this->css[] = $link;
+		$this->description = $description;
 	}
 	
-	public function addString(string $str)  // добавляет в массив для хранения
+	public function setKeywords(string $keywords): void
 	{
-		$this->string[] = $str;
+		$this->keywords = $keywords;
 	}
 	
-	public function setProperty(string $id, mixed $value) // добавляет для хранение
+	public function addJs(string $src): void  //добавляет src в массив сохраняя уникальность
+	{
+		if (!in_array($src, $this->js, true)) {
+			$this->js[] = $src;
+		}
+	}
+	
+	public function addCss(string $link): void  //добавляет link сохраняя уникальность
+	{
+		if (!in_array($link, $this->css, true)) {
+			$this->css[] = $link;
+		}
+	}
+	
+	public function addString(string $str): void  // добавляет в массив для хранения
+	{
+		if (!in_array($str, $this->string, true)) {
+			$this->string[] = $str;
+		}
+	}
+	
+	public function showTitle(): void
+	{
+		echo $this->macros['head']['title'];
+	}
+	
+	public function showDescription(): void
+	{
+		echo $this->macros['head']['description'];
+	}
+	
+	public function showKeywords(): void
+	{
+		echo $this->macros['head']['keywords'];
+	}
+	
+	public function showHead(): void // выводит 3 макроса замены CSS / STR / JS
+	{
+		echo $this->macros['head']['js'];
+		echo $this->macros['head']['css'];
+		echo $this->macros['head']['string'];
+	}
+	
+	public function setProperty(string $id, string $value): void // добавляет для хранения
 	{
 		$this->properties[$id] = $value;
 	}
 	
-	public function getProperty(string $id)  // получение по ключу
+	public function getProperty(string $id): string
 	{
 		return $this->properties[$id];
 	}
 	
-	public function showProperty(string $id)  // выводит макрос для будущей замены
+	public function showProperty(string $id): void  // выводит макрос для будущей замены
 	{
 		echo "#FW_PAGE_PROPERTY_$id#";
 	}
 	
-	public function getAllReplace() // получает массив макросов и значений для замены
+	public function getAllReplace(string $content): string
 	{
-		return
-			[
-				'js' => $this->js,
-				'css' => $this->css,
-				'string' => $this->string,
-				'properties' => $this->properties,
-			];
-		//return $this->properties;
-	}
-	
-	public function showHead() // выводит 3 макроса замены CSS / STR / JS
-	{
-		echo "#FW_MACRO_JS#";
-		echo "#FW_MACRO_CSS#";
-		echo "#FW_MACRO_STRING#";
+		$js = '';
+		if (!empty($this->js)) {
+			foreach ($this->js as $src) {
+				$js .= '<script async src="' . $src . '"></script>' . PHP_EOL;
+			}
+		}
+		$css = '';
+		if (!empty($this->css)) {
+			foreach ($this->css as $link) {
+				$css .= '<link href="' . $link . '" type="text/css" rel="stylesheet">' . PHP_EOL;
+			}
+		}
+		$string = '';
+		if (!empty($this->string)) {
+			$string = implode(PHP_EOL, $this->string);
+		}
+		$search_head = [];
+		foreach ($this->macros['head'] as $macro) {
+			$search_head[] = $macro;
+		}
+		$replace_head = [$this->title, $this->description, $this->keywords, $js, $css, $string];
+		$content = str_replace($search_head, $replace_head, $content);
+		
+		foreach ($this->properties as $id => $value) {
+			$content = str_replace("#FW_PAGE_PROPERTY_$id#", $value, $content);
+		}
+		
+		return preg_replace('/#FW_PAGE_PROPERTY_.*#/', '', $content);
 	}
 }
 
